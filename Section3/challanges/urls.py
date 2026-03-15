930846288a22e8bd00826b25d2b9be9aa2af4d18
unknown <experte001@gmail.com>
2026-03-15 09:31:04 +0900

    Path Converter

--- Section3/challanges/urls.py
+++ Section3/challanges/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('<int:month>', views.monthly_challange_by_number, name='monthly-challange-by-number'),
 	path('<str:month>', views.monthly_challange, name='monthly-challange'),
 ]
--- Section3/challanges/views.py
+++ Section3/challanges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse,Http404,HttpResponseNotFound
+from django.http import HttpResponse,Http404,HttpResponseNotFound,HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def january(request, *args, **kwargs):
@@ -26,7 +27,34 @@ def monthly_challange(request, month, *args, **kwargs):
 
 	challange_text = challanges.get(month, None)
 
+	print(type(month))
+
 	if challange_text == None:
 		return HttpResponseNotFound("This month is not supported.")
 	else:
 		return HttpResponse(challange_text)
+	
+def monthly_challange_by_number(request, month, *args, **kwargs):
+	challanges = {
+		"january": "This is January challange.",
+		"february": "This is February challange.",
+		"march": "This is March challange.",
+		"april": "This is April challange.",
+		"may": "This is May challange.",
+		"june": "This is June challange.",
+		"july": "This is July challange.",
+		"august": "This is August challange.",
+		"september": "This is September challange.",
+		"october": "This is October challange.",
+		"november": "This is November challange.",
+		"december": "This is December challange."
+	}
+	months = list(challanges.keys())
+
+	if month > len(months):
+		return HttpResponseNotFound("This month is not supported.")
+	else:
+		redirect_month = months[month-1]
+		redirect_path = reverse("monthly-challange", args=[redirect_month])
+		return HttpResponseRedirect(redirect_path)
+	
