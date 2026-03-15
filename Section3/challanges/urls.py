22e3e699355995386d2f913ac51e76013d711d76
unknown <experte001@gmail.com>
2026-03-15 09:23:33 +0900

    Dynamic Path Segment

--- Section3/challanges/urls.py
+++ Section3/challanges/urls.py
@@ -4,6 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('january', views.january, name='january'),
-	path('february', views.february, name='february'),
+	path('<str:month>', views.monthly_challange, name='monthly-challange'),
 ]
--- Section3/challanges/views.py
+++ Section3/challanges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404,HttpResponseNotFound
 
 # Create your views here.
 def january(request, *args, **kwargs):
@@ -7,3 +7,26 @@ def january(request, *args, **kwargs):
 
 def february(request, *args, **kwargs):
 	return HttpResponse("This is February challange.")
+
+def monthly_challange(request, month, *args, **kwargs):
+	challanges = {
+		"january": "This is January challange.",
+		"february": "This is February challange.",
+		"march": "This is March challange.",
+		"april": "This is April challange.",
+		"may": "This is May challange.",
+		"june": "This is June challange.",
+		"july": "This is July challange.",
+		"august": "This is August challange.",
+		"september": "This is September challange.",
+		"october": "This is October challange.",
+		"november": "This is November challange.",
+		"december": "This is December challange."
+	}
+
+	challange_text = challanges.get(month, None)
+
+	if challange_text == None:
+		return HttpResponseNotFound("This month is not supported.")
+	else:
+		return HttpResponse(challange_text)
