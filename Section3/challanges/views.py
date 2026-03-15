c848f08e74c2ebcd83e74b2d368b254940c3c320
unknown <experte001@gmail.com>
2026-03-15 09:36:22 +0900

    Adding More Dynamic View Logics

--- Section3/challanges/views.py
+++ Section3/challanges/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse,Http404,HttpResponseNotFound,HttpResponseRedirect
 from django.urls import reverse
 
+challanges = {
+	"january": "This is January challange.",
+	"february": "This is February challange.",
+	"march": "This is March challange.",
+	"april": "This is April challange.",
+	"may": "This is May challange.",
+	"june": "This is June challange.",
+	"july": "This is July challange.",
+	"august": "This is August challange.",
+	"september": "This is September challange.",
+	"october": "This is October challange.",
+	"november": "This is November challange.",
+	"december": "This is December challange."
+}
+
 # Create your views here.
 def january(request, *args, **kwargs):
 	return HttpResponse("This is January challange.")
@@ -10,47 +25,14 @@ def february(request, *args, **kwargs):
 	return HttpResponse("This is February challange.")
 
 def monthly_challange(request, month, *args, **kwargs):
-	challanges = {
-		"january": "This is January challange.",
-		"february": "This is February challange.",
-		"march": "This is March challange.",
-		"april": "This is April challange.",
-		"may": "This is May challange.",
-		"june": "This is June challange.",
-		"july": "This is July challange.",
-		"august": "This is August challange.",
-		"september": "This is September challange.",
-		"october": "This is October challange.",
-		"november": "This is November challange.",
-		"december": "This is December challange."
-	}
-
 	challange_text = challanges.get(month, None)
-
-	print(type(month))
-
 	if challange_text == None:
 		return HttpResponseNotFound("This month is not supported.")
 	else:
 		return HttpResponse(challange_text)
 	
 def monthly_challange_by_number(request, month, *args, **kwargs):
-	challanges = {
-		"january": "This is January challange.",
-		"february": "This is February challange.",
-		"march": "This is March challange.",
-		"april": "This is April challange.",
-		"may": "This is May challange.",
-		"june": "This is June challange.",
-		"july": "This is July challange.",
-		"august": "This is August challange.",
-		"september": "This is September challange.",
-		"october": "This is October challange.",
-		"november": "This is November challange.",
-		"december": "This is December challange."
-	}
 	months = list(challanges.keys())
-
 	if month > len(months):
 		return HttpResponseNotFound("This month is not supported.")
 	else:
