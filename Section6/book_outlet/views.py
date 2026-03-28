2afd373122e667f4f9a9ef44d2c72a4969c20f16
unknown <experte001@gmail.com>
2026-03-28 18:06:55 +0900

    Retrive real data from DB

--- Section6/book_outlet/views.py
+++ Section6/book_outlet/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Book
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+def index(request):
+	books=Book.objects.all()
+	context={
+		"books":books
+	}
+	return render(request,"book_outlet/index.html",context)
+
+def book_details(request,id):
+	#try:
+	#	book=Book.objects.get(id=id)
+	#except:
+	#	raise Http404()
+	book=get_object_or_404(Book,id=id)
+	context={
+	"title":book.title,
+	"rating":book.rating,
+	"is_bestseller":book.is_bestselling,
+	"author":book.author
+	}
+	return render(request,"book_outlet/book_details.html",context)
