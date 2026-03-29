fa26df7bec62aee8444805cd6d66f6570d809c5c
unknown <experte001@gmail.com>
2026-03-29 22:59:17 +0900

    One to One Relationship

--- Section6/book_outlet/admin.py
+++ Section6/book_outlet/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Book,Author
+from .models import Book,Author, Address	
+
+class AddressAdmin(admin.ModelAdmin):
+	list_display = ("street","postal_code","city")
+	search_fields = ("street","postal_code","city")
+	list_filter = ("street","postal_code","city")
+
 
 class BookAdmin(admin.ModelAdmin):
 	list_display = ("title","author","rating","is_bestselling")
@@ -16,3 +22,5 @@ class AuthorAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Book,BookAdmin)
 admin.site.register(Author,AuthorAdmin)
+admin.site.register(Address,AddressAdmin)
+#admin.site.register(Author)
--- Section6/book_outlet/models.py
+++ Section6/book_outlet/models.py
@@ -5,13 +5,30 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Address(models.Model):
+	street=models.CharField(max_length=100)
+	postal_code=models.CharField(max_length=20)
+	city=models.CharField(max_length=50)
+
+
+	def __str__(self):
+		return f"{self.street}, {self.postal_code}, {self.city}"
+
+
 class Author(models.Model):
 	first_name=models.CharField(max_length=100)
 	last_name=models.CharField(max_length=100)
 	bio=models.TextField()
+	address=models.OneToOneField(Address,on_delete=models.CASCADE,null=True,related_name="author_address")
+
+	def full_name(self):
+		return f"{self.first_name} {self.last_name}"
+
 
 	def __str__(self):
-		return f"{self.first_name} {self.last_name}"
+		return self.full_name()
+	
+
 
 class Book(models.Model):
 	title = models.CharField(max_length=100)
